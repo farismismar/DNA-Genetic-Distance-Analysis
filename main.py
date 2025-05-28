1e508275e224a8d4c0fcc9ccbc2d3bcee09a7a4a
farismismar <faris_mismar@hotmail.com>
2025-05-27 22:27:07 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from io import StringIO
 
 user_kit_id = '999999'  # Replace with your actual Y111 kit ID from FamilyTreeDNA
 output_file = "output.csv"  # Replace with your CSV file path
-url = 'https://www.familytreedna.com/public/J2-M172?iframe=ydna-results-overview'  # Replace with familytreedna.com website (or None)
+url = 'https://www.familytreedna.com/public/J2-Arab?iframe=ydna-results-overview'  # Replace with familytreedna.com website (or None)
 input_file = 'sample_familytreedna.csv'
 
 # Multi-copy find the max GD:
@@ -46,26 +46,27 @@ def retrieve_data(url):
     # This is an HTTP POST method.
     system_command = f"curl -s '{url}'"
     system_command = system_command + '\\' + '''
-        -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
-        -H 'Accept-Language: en-US,en;q=0.9,ar;q=0.8' \
-        -H 'Cache-Control: no-cache' \
-        -H 'Connection: keep-alive' \
-        -H 'Content-Type: application/x-www-form-urlencoded' \
-        -b 'CookieConsent={stamp:%27-1%27%2Cnecessary:true%2Cpreferences:true%2Cstatistics:true%2Cmarketing:true%2Cmethod:%27implied%27%2Cver:1%2Cutc:1721493826898%2Cregion:%27JO%27}; BNES_CookieConsent=lvxGolPiBv1B84jtPTDcV9LSyOoydpLJpCtYbQeDiHNVK9C8jiyjszjFZdUFsSLx8jdwoHJlJNiEE0ou8rFeIaODoQpS6IN/h8+QpMIYO30OGUYNP4Dt0SJwBqijSj1vOb595P1DD3FjJLtrWGoyQBLtpzNI4C/8Oe7CtSgDa6uSDhX5s21uRNz76yDfnoRHrtDQ6eaSFqmPzXX9GB3wnJhDNfcSEqzLrzf9MEVOd3arE6Qz+0U2fZKkBEvuaf9WTP27qQnEY34=; _gcl_au=1.1.1029900071.1721493831; __zlcmid=1NFmwZbeiA1OqsF; __zlcmid=1NFmwZbeiA1OqsF; BNES__gcl_au=IMY4/RYeqpi3hoXQ0YhSdcA5jLHS1v+7+W59G14Uqp6nyHI5N5y5ZhMMrrOhBaIUdsbcGuf3ZfM9aDWWu36UIA==; cf_clearance=8wJ6nzT7CF.D21_xTe8hQh_m7UFHNDXFs4d5_8I7qeE-1748274313-1.2.1.1-mwTGdMS9UXffBesdhms.Zdg56YcKKrB98qky.qgzBmE7lIOzJ53RcUW5oPuAkMREh63itlPRF3s.vpYXlWXwlUzPlbb_2rLBBzU9E7d7FVK133hydw6J1ILcRBYOiVw8PtJo4I6mXAJPbegQ0fiz2AZ0FSBq78x.nz.LBdPRMKry9NeAblMShPS8b5y.tIaj0OZz3UIhtVQ_pVK5PVZqT4j..JRp5uLloG2.0ACmH3BGJQ2rjBZLU9_l7EKl7KOlMArWfCVf6iv4EA2O361VEg8PDZnInGXhw2vdSOH8rEZNRmBv48sLLC9mb9Es9RZ2fGTfLvJpirJHMTx2WrskLUPPyG8eHXPynrtlkZDc18g; cf_clearance=8wJ6nzT7CF.D21_xTe8hQh_m7UFHNDXFs4d5_8I7qeE-1748274313-1.2.1.1-mwTGdMS9UXffBesdhms.Zdg56YcKKrB98qky.qgzBmE7lIOzJ53RcUW5oPuAkMREh63itlPRF3s.vpYXlWXwlUzPlbb_2rLBBzU9E7d7FVK133hydw6J1ILcRBYOiVw8PtJo4I6mXAJPbegQ0fiz2AZ0FSBq78x.nz.LBdPRMKry9NeAblMShPS8b5y.tIaj0OZz3UIhtVQ_pVK5PVZqT4j..JRp5uLloG2.0ACmH3BGJQ2rjBZLU9_l7EKl7KOlMArWfCVf6iv4EA2O361VEg8PDZnInGXhw2vdSOH8rEZNRmBv48sLLC9mb9Es9RZ2fGTfLvJpirJHMTx2WrskLUPPyG8eHXPynrtlkZDc18g; BNI_ServerId=LrUOKKYnbY6Z-DbMpDGcwJWQp16z_Mcg2BCg1-I8WIuq8fMIvVTqwiJMBC2YQUJydXStFiROfNpaT-j2F8hNfg==; ASP.NET_SessionId=ntctgsoqrj5eymxq1cxvkzym; BNES_ASP.NET_SessionId=Z9eqYEiEL2iIG5B27dIrD1cLjpwOdQmOIWYhhuzY543bvyNnLlgdkVF4t1bQdLeX4pjE5OvdE2RqZRd61DER+o/3N2vFkalY; BNES___zlcmid=YmOnEVUgkiv70J+H0t6DbyftafJRK9tSt8T9Tctk3zh4Ff+COZniAIF0zHWpXsZZuZJt8EQRO30=; BNES_cf_clearance=/c80ZqLa5yTXUGQfy8xyeyM2Wal57/dtJnXIDAe4lApPkXwPMGN7pYSscSwfhLt5qSeT/atkO6TPXYAlkhFE8He5BUf5+wvlm70bkf9VRZxZYXt3aTtBe3TsDCWRNIv7gAjxM8XkCHtscSP9VVs/O0kRnn+l+S6cXy1n+SO28eN/EhUWJts7RPXcb3sAPaNyLVGghjdAImC1SSgGAutOaksP5JLpJwaGSiBIDmaBShIpLqV1d01H+vdcCkNQUtwQQQiNFbE8hSEjkAWirOZV6O3IKHQWSm220XWr3VUEqYPsHvUNOiX5omjHunS+l/teTEw1obKqVbAV1nO1e+rPlbkpMJQujv6P6FRuSCmPAat7zHYuT0lFyBSgslneKy/4vjgq5ANd941c7xz8Pb++ndvlXlhYTd74PzohJ4aBv+/sERdDrQxjKmujVywd5nZerowp+uZGjLuiGn+iYnyn+wZ3KlvWPxfQj6nfM9w0wwAYad3NsTHI8siM6t1KTrl53K+TDt3PJsf4QFcoWMu5B/BRlBLJQjI/KWtnHxr7Ftag1FXTuL+ZyDHfXJAXj3w+JuzFnioipNR3X0YeWQLKW7CgtNB/zrY80UGrYKFiSqeXxwirOJTtew==' \
-        -H 'DNT: 1' \
-        -H 'Origin: https://www.familytreedna.com' \
-        -H 'Pragma: no-cache' \
-        -H 'Referer: {url}' \
-        -H 'Sec-Fetch-Dest: document' \
-        -H 'Sec-Fetch-Mode: navigate' \
-        -H 'Sec-Fetch-Site: same-origin' \
-        -H 'Sec-Fetch-User: ?1' \
-        -H 'Upgrade-Insecure-Requests: 1' \
-        -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36' \
-        -H 'sec-ch-ua: "Chromium";v="136", "Google Chrome";v="136", "Not.A/Brand";v="99"' \
-        -H 'sec-ch-ua-mobile: ?0' \
-        -H 'sec-ch-ua-platform: "macOS"'
-      '''
+      -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+      -H 'Accept-Language: en-US,en;q=0.9,ar;q=0.8' \
+      -H 'Cache-Control: no-cache' \
+      -H 'Connection: keep-alive' \
+      -H 'Content-Type: application/x-www-form-urlencoded' \
+      -b 'CookieConsent={stamp:%27-1%27%2Cnecessary:true%2Cpreferences:true%2Cstatistics:true%2Cmarketing:true%2Cmethod:%27implied%27%2Cver:1%2Cutc:1721493826898%2Cregion:%27JO%27}; BNES_CookieConsent=lvxGolPiBv1B84jtPTDcV9LSyOoydpLJpCtYbQeDiHNVK9C8jiyjszjFZdUFsSLx8jdwoHJlJNiEE0ou8rFeIaODoQpS6IN/h8+QpMIYO30OGUYNP4Dt0SJwBqijSj1vOb595P1DD3FjJLtrWGoyQBLtpzNI4C/8Oe7CtSgDa6uSDhX5s21uRNz76yDfnoRHrtDQ6eaSFqmPzXX9GB3wnJhDNfcSEqzLrzf9MEVOd3arE6Qz+0U2fZKkBEvuaf9WTP27qQnEY34=; _gcl_au=1.1.1029900071.1721493831; __zlcmid=1NFmwZbeiA1OqsF; __zlcmid=1NFmwZbeiA1OqsF; BNES__gcl_au=IMY4/RYeqpi3hoXQ0YhSdcA5jLHS1v+7+W59G14Uqp6nyHI5N5y5ZhMMrrOhBaIUdsbcGuf3ZfM9aDWWu36UIA==; cf_clearance=8wJ6nzT7CF.D21_xTe8hQh_m7UFHNDXFs4d5_8I7qeE-1748274313-1.2.1.1-mwTGdMS9UXffBesdhms.Zdg56YcKKrB98qky.qgzBmE7lIOzJ53RcUW5oPuAkMREh63itlPRF3s.vpYXlWXwlUzPlbb_2rLBBzU9E7d7FVK133hydw6J1ILcRBYOiVw8PtJo4I6mXAJPbegQ0fiz2AZ0FSBq78x.nz.LBdPRMKry9NeAblMShPS8b5y.tIaj0OZz3UIhtVQ_pVK5PVZqT4j..JRp5uLloG2.0ACmH3BGJQ2rjBZLU9_l7EKl7KOlMArWfCVf6iv4EA2O361VEg8PDZnInGXhw2vdSOH8rEZNRmBv48sLLC9mb9Es9RZ2fGTfLvJpirJHMTx2WrskLUPPyG8eHXPynrtlkZDc18g; cf_clearance=8wJ6nzT7CF.D21_xTe8hQh_m7UFHNDXFs4d5_8I7qeE-1748274313-1.2.1.1-mwTGdMS9UXffBesdhms.Zdg56YcKKrB98qky.qgzBmE7lIOzJ53RcUW5oPuAkMREh63itlPRF3s.vpYXlWXwlUzPlbb_2rLBBzU9E7d7FVK133hydw6J1ILcRBYOiVw8PtJo4I6mXAJPbegQ0fiz2AZ0FSBq78x.nz.LBdPRMKry9NeAblMShPS8b5y.tIaj0OZz3UIhtVQ_pVK5PVZqT4j..JRp5uLloG2.0ACmH3BGJQ2rjBZLU9_l7EKl7KOlMArWfCVf6iv4EA2O361VEg8PDZnInGXhw2vdSOH8rEZNRmBv48sLLC9mb9Es9RZ2fGTfLvJpirJHMTx2WrskLUPPyG8eHXPynrtlkZDc18g; BNI_ServerId=LrUOKKYnbY6Z-DbMpDGcwJWQp16z_Mcg2BCg1-I8WIuq8fMIvVTqwiJMBC2YQUJydXStFiROfNpaT-j2F8hNfg==; ASP.NET_SessionId=5scmqbxjqh0glrrnf4dgwufq; BNES_ASP.NET_SessionId=vEewSvtiTJw5SBW2HZh4zntJv4Vzyrev8lcINyLIPaITvJvDTvElMyTnOoAfDIm4K0yNOAz7U+pOnNo3cJDdBUUreWXh7YBh; BNES___zlcmid=qlIzSC3mboMGlowQoED5dCQXM0aEyw6JmKz6B9JPgZn6nzNtnv7wi6i0NS6kt/M6K7EZS14cXkE=; BNES_cf_clearance=A+lszrbaVfem5BctKdiQ5CJlAiNlSpDngr0jaqOHX41nKE+kqvSJKG/7KLiXhOPUZXpLO7GZt1WtomNrnMWhDmdmt2ZT1Mzzg3M0nqaknuV0veP8DkdKwXlo4k8SCK7ALxc92ya0SToivAshsudIuFsOeV1opCYzPQUp/jugT3C+VAkcAl6gn3SwZpcfEbZCLXo0yNSHKHfFnXAfRJcG++13k0pQdPiStL70ox4jXlIZcNDx1qvu9kvlA3S+BXqVPnaD4/L2qHkAy6TyeuYabWuzYr4KJhQj0DLN1EsQDnt4JvxmChGg4DD4y/3tDj7cb9K7K/pd5nMqO4ghQceGXM5bNqKDIsYvwLdsUDZlXro0LAQXV6/CdQJb5bIlnRvvrURjJF3rPVYqhLKF9saHHsUZ2LvO+2GJSx9vScQ/dyo3i7ci82qj8wBjS3Nq/TwLHzpEZOtoCDeHE8smVpSBljElHu0BIBBh5x8SGgbf9cqH6MXmKbhyuVAlJcfrCZC1WdPJkFSFZp1QghTrAqq+ajREvPWnOZrtH0602XnDBkVBUIYAKgGYUSgngN1rC+MBR/smRpH5GwAPObIgQuprt3y0THMxgXH9JSvxkyVjC26gYzp4sd0sBA==' \
+      -H 'DNT: 1' \
+      -H 'Origin: https://www.familytreedna.com' \
+      -H 'Pragma: no-cache' \
+      -H 'Referer: {url}' \
+      -H 'Sec-Fetch-Dest: document' \
+      -H 'Sec-Fetch-Mode: navigate' \
+      -H 'Sec-Fetch-Site: same-origin' \
+      -H 'Sec-Fetch-User: ?1' \
+      -H 'Upgrade-Insecure-Requests: 1' \
+      -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36' \
+      -H 'sec-ch-ua: "Chromium";v="136", "Google Chrome";v="136", "Not.A/Brand";v="99"' \
+      -H 'sec-ch-ua-mobile: ?0' \
+      -H 'sec-ch-ua-platform: "macOS"' \
+      --data "@post_data.txt"
+    '''
   
     try:
         html_output = os.popen(system_command).read()
